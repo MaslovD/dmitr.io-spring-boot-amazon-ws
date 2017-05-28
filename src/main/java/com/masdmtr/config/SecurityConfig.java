d63d88e29d0e2333f9b63e77d9b382b11b214a29
dmaslov <masdmtr@gmail.com>
2017-05-28 15:24:38 +0300

    Initial commit

--- src/main/java/com/masdmtr/config/SecurityConfig.java
+++ src/main/java/com/masdmtr/config/SecurityConfig.java
@@ -1,23 +1,40 @@
 package com.masdmtr.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private static String REALM="MY_TEST_REALM";
 
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
-                .antMatchers("/students/3").hasRole("USER")
+              .antMatchers("/students/3").hasRole("USER")
                 .and()
                 .formLogin();
+       /* httpSecurity.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/students*//**").hasRole("ADMIN")
+                .and().httpBasic().realmName(REALM).authenticationEntryPoint(getBasicAuthEntryPoint())
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//We don't need sessions to be created.
+*/
+}
+
+    @Bean
+    public CustomBasicAuthenticationEntryPoint getBasicAuthEntryPoint(){
+        return new CustomBasicAuthenticationEntryPoint();
     }
+
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
